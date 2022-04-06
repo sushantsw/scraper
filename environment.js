4a5cf5ea6c87abd4203b4bd6e2a4d078c9ff1b65
Sushant Parashar <sushant.parashar@startegywerks.com>
2022-04-06 19:47:23 +0530

    fix

--- environment.js
+++ environment.js
@@ -1,10 +1,10 @@
 const environment = {
-  // apiUrl: 'http://localhost:3000/api/',
-  apiUrl: 'http://ec2co-ecsel-1jy3u5xjegjgd-771036687.us-east-2.elb.amazonaws.com/api',
+  apiUrl: 'http://localhost:3000/api/',
+  // apiUrl: 'http://ec2co-ecsel-1jy3u5xjegjgd-771036687.us-east-2.elb.amazonaws.com/api/',
   cognitoUrl: 'https://cognito-idp.us-east-2.amazonaws.com/',
   cognitoClientId: '5tt5ioq8rpvo29l8lbl78mtrkq',
   site: 'PINTEREST',
-  accountType: 'swimming',
+  accountType: 'cooking',
   postPerSearch: 10
 }
 
